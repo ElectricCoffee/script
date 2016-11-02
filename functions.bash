fdd1d7b240cfa0ef2d82c350ff56ead2a412a9a4
Niko Lepka <slench102+git@gmail.com>
2016-11-02 11:38:28 +0100

    add dir-exists
    ensures a sub-directory exists before creating the files

--- functions.bash
+++ functions.bash
@@ -38,32 +38,56 @@ function mkcd {
     cd $1
 }
 
+function dir-exists {
+    if [ ! -d $1 -a -n $1 ]
+    then
+	echo ">> Directory '$directory' doesn't exist, create it?"
+	select yn in Yes No
+	do
+	    case $yn in
+		Yes)
+		    echo ">> Creating $directory."
+		    mkdir $1
+		    return 0 # 0 is true
+		    ;;
+		No)
+		    echo ">> No action taken."
+		    return 1 # 1 is false
+		    ;;
+	    esac
+	done
+    fi
+
+    return 0
+}
+
 function touch-cpp {
     for file in "$@"
     do
 	local classname=$(basename $file)
-	local directory=$(dirname $file)
+	local directory=$(dirname  $file)
 	local cpp="$file.cpp"
 	local hpp="$file.hpp"
-	if [ ! -d $directory ]
-	then
-	    echo ">> Directory '$directory' doesn't exist, no action taken."
-	elif [ -f $cpp -o -f $hpp ]
-	then
-	    echo ">> Either $cpp or $hpp already exists, no action taken."
-	else
-	    touch $cpp
-	    echo "// The implementation of file $classname" > $cpp
-	    echo "#include \"$hpp\"" >> $cpp
 
-	    touch $hpp
-	    echo "// The header file of $classname" > $hpp
-	    echo "#ifndef ${classname}_hpp" >> $hpp
-	    echo "#define ${classname}_hpp" >> $hpp
-	    echo "// Code goes here"        >> $hpp
-	    echo "#endif"                   >> $hpp
-
-	    echo ">> Files $cpp and $hpp written to $directory/."
+	if dir-exists $directory
+	then	
+	    if [ -f $cpp -o -f $hpp ]
+	    then
+		echo ">> Either $cpp or $hpp already exists, no action taken."
+	    else
+		touch $cpp
+		echo "// The implementation of file $classname" > $cpp
+		echo "#include \"$hpp\"" >> $cpp
+		
+		touch $hpp
+		echo "// The header file of $classname" > $hpp
+		echo "#ifndef ${classname}_hpp" >> $hpp
+		echo "#define ${classname}_hpp" >> $hpp
+		echo "// Code goes here"        >> $hpp
+		echo "#endif"                   >> $hpp
+		
+		echo ">> Files $cpp and $hpp written to $directory/."
+	    fi
 	fi
     done
 }
