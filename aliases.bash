fa62bf213b5f3e8e4a835cb5450fad8454090cdc
Niko Lepka <slench102+git@gmail.com>
2016-11-02 10:19:02 +0100

    add fuckit

--- aliases.bash
+++ aliases.bash
@@ -4,4 +4,8 @@ alias sudo="sudo "
 
 alias install="apt-get install -y"
 
+alias uninstall="apt-get purge"
+
 alias gstat="git status"
+
+alias fuckit='git commit -am "$(curl http://whatthecommit.com/index.txt)"'
